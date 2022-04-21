da1c8f87d620d8f81d37aa62f884007380a2da96
Ciragane Nicole <ciraganenicole@gmail.com>
2022-04-21 18:05:04 +0200

    Refactor code by using functional component

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,62 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.clickHandle = this.clickHandle.bind(this);
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-  clickHandle(e) {
-    this.setState((state) => ({
+  const clickHandle = (e) => {
+    setState((state) => ({
       ...state,
       ...calculate(state, e.target.name),
     }));
-  }
+  };
+  const { total, next, operation } = state;
+  return (
+    <main>
+      <section>
+        <div className="row">
+          <div className="grid-item item2">
+            {total}
+            {operation}
+            {next}
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <main>
-        <section>
-          <div className="row">
-            <div className="grid-item item2">
-              {total}
-              {operation}
-              {next}
-
-            </div>
-
-            <button type="button" className="grid-item" onClick={this.clickHandle} name="AC">AC </button>
-            <button type="button" className="grid-item" onClick={this.clickHandle} name="+/-"> +/- </button>
-            <button type="button" className="grid-item" onClick={this.clickHandle} name="%"> % </button>
-            <button type="button" className="grid-item orange" onClick={this.clickHandle} name="÷"> ÷ </button>
-            <button type="button" className="grid-item" onClick={this.clickHandle} name="7"> 7 </button>
-            <button type="button" className="grid-item" onClick={this.clickHandle} name="8"> 8 </button>
-            <button type="button" className="grid-item" onClick={this.clickHandle} name="9"> 9 </button>
-            <button type="button" className="grid-item orange" onClick={this.clickHandle} name="x"> x </button>
-            <button type="button" className="grid-item" onClick={this.clickHandle} name="4"> 4 </button>
-            <button type="button" className="grid-item" onClick={this.clickHandle} name="5"> 5 </button>
-            <button type="button" className="grid-item" onClick={this.clickHandle} name="6"> 6 </button>
-            <button type="button" className="grid-item orange" onClick={this.clickHandle} name="-"> - </button>
-            <button type="button" className="grid-item" onClick={this.clickHandle} name="1"> 1 </button>
-            <button type="button" className="grid-item" onClick={this.clickHandle} name="2"> 2 </button>
-            <button type="button" className="grid-item" onClick={this.clickHandle} name="3"> 3 </button>
-            <button type="button" className="grid-item orange" onClick={this.clickHandle} name="+"> + </button>
-            <button type="button" className="grid-item item1" onClick={this.clickHandle} name="0"> 0 </button>
-            <button type="button" className="grid-item" onClick={this.clickHandle} name="."> . </button>
-            <button type="button" className="grid-item orange" onClick={this.clickHandle} name="="> = </button>
           </div>
-        </section>
-      </main>
-    );
-  }
-}
+
+          <button type="button" className="grid-item" onClick={clickHandle} name="AC">AC </button>
+          <button type="button" className="grid-item" onClick={clickHandle} name="+/-"> +/- </button>
+          <button type="button" className="grid-item" onClick={clickHandle} name="%"> % </button>
+          <button type="button" className="grid-item orange" onClick={clickHandle} name="÷"> ÷ </button>
+          <button type="button" className="grid-item" onClick={clickHandle} name="7"> 7 </button>
+          <button type="button" className="grid-item" onClick={clickHandle} name="8"> 8 </button>
+          <button type="button" className="grid-item" onClick={clickHandle} name="9"> 9 </button>
+          <button type="button" className="grid-item orange" onClick={clickHandle} name="x"> x </button>
+          <button type="button" className="grid-item" onClick={clickHandle} name="4"> 4 </button>
+          <button type="button" className="grid-item" onClick={clickHandle} name="5"> 5 </button>
+          <button type="button" className="grid-item" onClick={clickHandle} name="6"> 6 </button>
+          <button type="button" className="grid-item orange" onClick={clickHandle} name="-"> - </button>
+          <button type="button" className="grid-item" onClick={clickHandle} name="1"> 1 </button>
+          <button type="button" className="grid-item" onClick={clickHandle} name="2"> 2 </button>
+          <button type="button" className="grid-item" onClick={clickHandle} name="3"> 3 </button>
+          <button type="button" className="grid-item orange" onClick={clickHandle} name="+"> + </button>
+          <button type="button" className="grid-item item1" onClick={clickHandle} name="0"> 0 </button>
+          <button type="button" className="grid-item" onClick={clickHandle} name="."> . </button>
+          <button type="button" className="grid-item orange" onClick={clickHandle} name="="> = </button>
+        </div>
+      </section>
+    </main>
+  );
+};
 
 export default Calculator;
