2cc20c1b16f622b905f58c272154d456036926a8
Ciragane Nicole <ciraganenicole@gmail.com>
2022-04-27 15:05:58 +0200

    Create the home page

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from './Header';
+
+const Home = () => (
+  <main>
+    <Header />
+    <div className="main">
+      <h1 className="title">Welcome to our page</h1>
+      <p className="paragraph">
+        Math magicians is a website for all fans of mathematics. It is a Single
+        Page App (SPA) that allows users to make simple calculations and read a
+        random math-related quote. Math magicians is a website for all fans of
+        mathematics. It is a Single Page App (SPA) that allows users to make
+        simple calculations and read a random math-related quote. Math magicians
+        is a website for all fans of mathematics. It is a Single Page App (SPA)
+        that allows users to make simple calculations and read a random
+        math-related quote.
+      </p>
+      <p className="paragraph">
+        Math magicians is a website for all fans of mathematics. It is a Single
+        Page App (SPA) that allows users to make simple calculations and read a
+        random math-related quote. Math magicians is a website for all fans of
+        mathematics. It is a Single Page App (SPA) that allows users to make
+        simple calculations and read a random math-related quote. Math magicians
+        is a website for all fans of mathematics. It is a Single Page App (SPA)
+        that allows users to make simple calculations and read a random
+        math-related quote.
+      </p>
+    </div>
+  </main>
+);
+export default Home;
