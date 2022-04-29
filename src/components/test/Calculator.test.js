aecb5b393981062468bf6c4ff7364b142c0c89bd
Ciragane Nicole <ciraganenicole@gmail.com>
2022-04-29 09:26:07 +0200

    Fix linters errors

--- src/components/test/Calculator.test.js
+++ src/components/test/Calculator.test.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
